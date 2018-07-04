182797d17fe1e0e37cfd64e5db1c7d1a08036413
Sean Fox <geekypghrunner@gmail.com>
2018-07-04 14:48:03 -0400

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Saint Vincent College, Opportunity/ACT101/SSS-TRiO Program</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Budget Maker</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
